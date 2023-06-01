ea65dff63020aa99a58f5ddf64f42c60d0e36513
Tobyn Neame <tjneame@gmail.com>
2023-06-01 12:52:21 -0600

    Coppied and modified code from sent prey analysis. Still data management

--- beetleDataAnalysis.R
+++ beetleDataAnalysis.R
@@ -1,1 +1,125 @@
 # TITLE: beetleDataAnalysis
+# Author: Tobyn Neame
+
+#SET UP ------------------------------------------------------------------------
+
+library(tidyverse)
+library(sf)
+library(gamlss)
+library(mgcv)
+
+theme_set(theme_bw())
+
+# Functions from Sam 
+geom2cols <- function(d,x=lon,y=lat,removeGeom=TRUE,epsg=NA){
+  require(sf); require(dplyr)
+  if(!('sf' %in% class(d))) stop('Data must be an sf object')
+  if(is_grouped_df(d)){ #If data are grouped
+    warning('Data are grouped. Will ungroup data before converting 
+geometry column.')
+    d <- ungroup(d)
+  }
+  if(!is.na(epsg)) d <- st_transform(d,epsg) #Transform to new CRS
+  d <- d %>% 
+    mutate({{x}}:=st_coordinates(.)[,1],{{y}}:=st_coordinates(.)[,2]) #Make 
+  #new columns from coordinates
+  if(removeGeom) d <- st_drop_geometry(d) #Drop geometry
+  return(d)
+}
+
+#read in local info
+setwd("/Users/tobynneame/Documents/School/MastersData/beetleDataAnalysis")
+beetDat<-read_csv("beetleDataClean.csv")
+
+#get stuff from other locations
+setwd("~/Documents/School/MastersData/SentinelPrey")
+
+#Read in shapefiles
+fieldWhole <- read_sf("FieldWhole2.shp") %>% st_transform(3402)
+grass <- read_sf("Grass2.shp") %>% st_transform(3402)
+infra <- read_sf("Infrastructure2.shp") %>% st_transform(3402)
+trees <- read_sf("Trees2.shp") %>% st_transform(3402)
+wet <- read_sf("Wetlands2.shp") %>% st_transform(3402)
+
+#Read in csvs
+sampleLocs <- read.csv('SampleSiteLocations.csv',stringsAsFactors = FALSE) %>% 
+  mutate(lat_dup=lat,lon_dup=lon) %>%
+  st_as_sf(coords=c('lon','lat')) %>% #Add spatial feature info
+  st_set_crs(4326) %>%
+  st_transform(3402) %>%
+  geom2cols(.,x=lon_dup, y=lat_dup, removeGeom = FALSE, epsg=3402)
+
+ACISDat <- read.csv('ACISDailyData-20210601-20220831-PID131006430.csv')
+
+sentPrey <- read.csv('SentinelPreyCaterpillars.csv',stringsAsFactors = FALSE)
+
+#Take a look ------------------------------------
+ggplot(fieldWhole)+geom_sf()
+ggplot(grass)+geom_sf()
+ggplot(infra)+geom_sf()
+ggplot(trees)+geom_sf()
+
+#Take a look at BLID 41121
+ggplot()+
+  geom_sf(data=filter(fieldWhole,BLID==41121))+ #Field
+  geom_sf(data=filter(grass,BLID==41121),fill='green')+ #Grass
+  geom_sf(data=filter(trees,BLID==41121),fill='darkgreen')+ #Trees
+  geom_sf(data=filter(wet,BLID==41121), fill='darkblue')+ #Wetland
+  geom_sf(data=filter(sampleLocs,BLID==41121))
+
+# Figure out the distance from each point to boundary lines of grass/tree polygons ---------------
+
+# #Quicker example at a single field
+# treeLine <- st_cast(filter(trees,BLID==41021),'MULTILINESTRING')
+# grassLine <- st_cast(filter(grass,BLID==41021),'MULTILINESTRING')
+# treeGrassLine <- st_union(treeLine,grassLine)
+# 
+# ggplot()+
+#   geom_sf(data=filter(fieldWhole,BLID==41021))+ #Field
+#   geom_sf(data=filter(grass,BLID==41021),fill='green')+ #Grass
+#   geom_sf(data=filter(trees,BLID==41021),fill='darkgreen')+ #Trees
+#   geom_sf(data=filter(sampleLocs,BLID==41021))+
+#   geom_sf(data=treeGrassLine,col='red')
+# 
+# filter(sampleLocs,BLID==41021) %>% 
+#   mutate(dist=apply(st_distance(filter(sampleLocs,BLID==41021),treeGrassLine),1,min))
+
+#Turns polygons into lines (multi-line strings)
+treeLine <- st_cast(trees,'MULTILINESTRING')
+grassLine <- st_cast(grass,'MULTILINESTRING')
+treeGrassLine <- st_union(treeLine,grassLine) #Joins grass/trees together into a single object
+
+#Gets distances from tree/grass lines - TAKES A WHILE
+distMat <- st_distance(sampleLocs,treeGrassLine) #Gets distances from sample locations to grass/tree lines in a matrix
+
+minDist <- apply(distMat,1,min) #Gets minimum distances from each row (each point)
+
+sampleLocs <- sampleLocs %>%  mutate(dist=minDist) #Joins minDist into sampleLocs dataframe
+
+#Merges sample distances with prey records
+sampleLocs <- sampleLocs %>% unite(col = trapID, c(BLID,stationID),sep='-',remove = FALSE) #Creates a "Trap ID" column
+
+#Merges the tables together by "trapID" 
+beetDatDist <- st_drop_geometry(sampleLocs) %>%
+  dplyr::select(trapID, lon_dup, lat_dup, dist) %>% 
+  full_join(beetDat,by = 'trapID')
+
+# Add in growing degree data -------------------------------------------
+
+#merges beetle data and sentinel prey to gain weather station and mid date columns
+wStnDat <- sentPrey %>% unite(col = trapPassID, c(BLID,site,return),sep='-',remove = FALSE) %>% #Creates a "trapPassID" column
+  dplyr::select(trapPassID,weatherStn,midDate) %>% #makes a wather station data set from sent prey
+  distinct(.keep_all = TRUE)
+
+beetDatDist <- beetDatDist %>% unite(col = trapPassID, c(BLID,station,pass),sep="-",remove = FALSE) #Creates a "trapPassID" column
+
+beetDatTry1<- left_join(beetDatDist, wStnDat, by = 'trapPassID')
+
+#START HERE on Monday. Notes: some elytra lengths are in pixels, these will have to be fixed and cleaning code will have to be re-run. Good luck.
+
+#Merges prey records and sample distances with GDD data
+beetDatDist <- beetDatDist %>% unite(col = stnDate, c(weatherStn,midDate),sep='-',remove = FALSE) #Creates a "station date" column called stnDate
+ACISDat <- ACISDat %>% unite(col=stnDate, c(Station_Name,Date), sep = "-", remove = FALSE) #Creates a "station date" column called stnDate
+
+#Merges the tables together by "stnDate"
+beetDatDistGDD <- left_join(beetDatDist, ACISDat, by = 'stnDate')
