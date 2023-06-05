e711da80ada4ab14a67c5b88a3a3a9cfd61a196c
Tobyn Neame <tjneame@gmail.com>
2023-06-05 16:37:16 -0600

    More visuals with jitter and transparency

--- beetleDataAnalysis.R
+++ beetleDataAnalysis.R
@@ -23,8 +23,32 @@ beetDat <- beetDat %>%
 
 #Size by distance
 beetDat %>%
-  ggplot(aes(x=dist,y=elytraLength))+geom_point() #Plots bite marks by distance
+  ggplot(aes(x=dist,y=elytraLength))+geom_point() #Plots elytra size by distance
+
+#Size by distance just P.mel
+beetDatPMel<-dplyr::filter(beetDat, species=='melanarius')
+beetDatPMel %>%
+  ggplot(aes(x=dist,y=elytraLength))+geom_point() #Plots elytra size of P.mel by distance
+
+#Size by distance, no P.mel
+beetDatNoP<-dplyr::filter(beetDat, is.na(genus))
+beetDatNoP %>%
+  ggplot(aes(x=dist,y=elytraLength))+geom_point() #Plots elytra size of beetles that arent P.mel by distance
 
 
+#Size by distance, late season
+beetDatLate<-dplyr::filter(beetDat, GDD>600)
+beetDatLate %>%
+  ggplot(aes(x=dist,y=elytraLength))+geom_point() #Plots elytra size of beetles that arent P.mel by distance
 
+#Size by distance, mid season
+beetDatMid<-dplyr::filter(beetDat, GDD<600, GDD>300)
+beetDatMid %>%
+  ggplot(aes(x=dist,y=elytraLength))+
+  geom_point()
+
+#Size by distance showing season time 
+beetDat %>%
+  ggplot(aes(x=dist,y=elytraLength))+
+  geom_point(alpha=.2, position=position_jitter(width = 2))
 
