eb819be2f59e8d05fc8ace646038f4d6d1ee0396
Tobyn Neame <tjneame@gmail.com>
2023-07-10 11:04:47 -0600

    Visualize and summarize crop/non-crop elytra length models

--- testVisualizeMods.R
+++ testVisualizeMods.R
@@ -366,10 +366,10 @@ predicted_m6D
 appraise(m6) #Probably not applicable because gratia may not know how to deal with SHASH distributions? See note below about QGAM?
 
 
-## QGAMS Supplementary code from Paul -----------------------------------------
+## QGAMS Supplementary code from Paul - Elytra length distance -----------------------------------------
 
 ## Set working directory to location of RDS files
-setwd("/Users/tobynneame/Documents/School/MastersData/beetleDataAnalysis/QGAMs")
+setwd("/Users/tobyn.neame/Documents/MScWorking/beetleDataAnalysis/QGAM")
 
 ## LOAD THE QGAM RDS OBJECTS
 ## PLACE THEM IN A LIST OBJECT
@@ -466,7 +466,7 @@ ggplot(prD) +
   ylab("Elytra length (mm)") +
   xlab("Distance from field margin (m)")
 
-#Try with smooth qgams --------------------------------------------------
+#Try with smooth qgams - elytra length distance --------------------------------------------------
 
 ## LOAD THE QGAM RDS OBJECTS
 ## PLACE THEM IN A LIST OBJECT
@@ -561,7 +561,7 @@ ggplot(prDSmooth) +
 
 
 
-#Visualize SHASH Models-------------------------------------------------------
+#Visualize elytra length distance SHASH Models-------------------
 ## Set working directory to location of RDS files
 setwd("/Users/tobynneame/Documents/School/MastersData/beetleDataAnalysis/SHASH")
 ## LOAD THE GAM RDS OBJECTS
@@ -724,3 +724,107 @@ newdat3 <- predict.gam(m5,newdata=newdat3,se.fit = TRUE,
     #xlim()+
     scale_color_manual(values=cols)+scale_fill_manual(values=cols)
 )
+
+#visualize crop/noncrop elytra qGAMS-------------------------------------------
+
+setwd("/Users/tobyn.neame/Documents/MScWorking/beetleDataAnalysis/QGAM")
+
+## LOAD THE QGAM RDS OBJECTS
+## PLACE THEM IN A LIST OBJECT
+objectsToLoad <- paste0("qGAM", 1:9, "LineNC.rds")
+allModels <- map(objectsToLoad, read_rds)
+names(allModels) <- objectsToLoad
+
+
+## Some quick tricks for visualizing results
+## These will work with any GAM or qGAM 
+
+## Look at only one of the smooths
+## from the fifth model (i.e. median)
+## This one shows the map of BLID 41007
+draw(smooth_estimates(allModels[[5]], "s(lon_dup,lat_dup):BLID41007"))
+## The pattern is:
+## draw(smooth_estimates(MODEL_GOES_HERE, "the exact smooth name as it appears in summary"))
+
+## Plotting the quantile smooths for distance
+
+## GRAB THE DATA THAT WAS USED TO CREATE THE MODEL FROM
+## THE FIRST QGAM OBJECT
+inD <- allModels[[1]]$model
+
+## SPECIFY THE QUANTILES OF GDD THAT YOU WANT TO PREDICT FOR
+## (0.43 is the mean in this dataset)
+quantilesGDD <- c(0.1, 0.43, 0.9)
+
+## SPECIFY THE DATA ON WHICH TO PREDICT THE SMOOTHS
+## These three levels of GDD are roughly the mean, and the
+## 25% and 75% quantiles of the GDD distribution.
+## (In other words: early season, mid-season and late-season)
+## If you change them you need to make sure the numbers
+## are consistent further down.
+pr_inD <- expand.grid(
+  station = c("Crop","nonCrop"),
+  GDD=c(200,400, 750)) %>%
+  bind_cols(
+    tibble(BLID="41014",
+           lon_dup=0,
+           lat_dup=0,
+           year="2022")) %>%
+  as_tibble()
+
+## PREDICT THE SMOOTHS
+## Note how exclude contains the same BLID interaction
+## as the the BLID that was used when predicting.
+## This is important!
+prD <- tibble(quantile=map(allModels, function(x) 
+  x$family$getQu()) %>% 
+    as.numeric(),
+  y_hat=map(allModels, function(x) 
+    predict(x, 
+            newdata = pr_inD, 
+            exclude=c("s(BLID)", "s(lon_dup,lat_dup):BLID41014"),
+            type="response", se.fit=TRUE) %>%
+      bind_cols() %>%
+      bind_cols(pr_inD))) %>%
+  unnest(y_hat)
+
+## Draw the quantiles at the chosen levels of GDD
+ggplot(prD) +
+  geom_line(aes(x=station, y=fit, colour=quantile, group=quantile)) + 
+  facet_wrap(~as.factor(GDD)) 
+
+
+## Prepare the raw data for plotting using three GDD panels
+## identifying which data point is within a specified GDDs of
+## the panel mean for plotting
+## Here the GDD breaks are 200, 400 and 750, and any point sampled
+## more than 75 GDDs on either side of those breaks is removed
+## If you change the breaks here, you must change them above too.
+inD2 <- inD %>%
+  mutate(fA=ifelse(abs(200-GDD) < 75, 200, 0),
+         fB=ifelse(abs(400-GDD) < 75, 400, 0),
+         fC=ifelse(abs(750-GDD) < 75, 750, 0)) %>%
+  mutate(GDD=fA+fB+fC) %>%
+  dplyr::select(-c(fA, fB, fC)) %>%
+  filter(GDD != 0)
+
+
+
+## A three-panel plot showing the beetles collected within +/- 75 GDDs
+## of each of the three times, and the relationship with distance
+## at nine different deciles.
+ggplot(prD) +
+  geom_line(aes(x=station, y=fit, colour=quantile, group=quantile), size=1.25) +
+  geom_jitter(data=inD2, aes(x=station, y=elytraLength), 
+              alpha=0.05, width=.05, height=0, colour="black") + 
+  facet_wrap(~as.factor(GDD), ncol=3) +
+  theme_light() +
+  scale_colour_viridis_c(option="E") +
+  coord_cartesian(ylim=c(1.75,11)) +
+  ylab("Elytra length (mm)") +
+  xlab("Crop vs non-crop")
+
+#Summary crop/noncrop elytra SHASH--------------------------------------------------
+setwd("C:/Users/tobyn.neame/Documents/MScWorking/beetleDataAnalysis")
+m20<-read_rds("cropNonCrop_GAMSHASH_20.rds")
+summary(m20)
